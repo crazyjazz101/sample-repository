835e9383dff7ff3850c5d424962d1a270235aba1
Govind Thawkar <thawkargovind192@gmail.com>
2024-02-16 09:53:07 +0530

    sample-repository

---
+++ index.js
@@ -0,0 +1,28 @@
+const hexNumbers = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F"
+];
+
+function changeBackgroundColor() {
+  let hexColorCode = "";
+  for (var i = 0; i < 6; i++) {
+    let randomIndex = Math.floor(Math.random() * hexNumbers.length);
+    hexColorCode += hexNumbers[randomIndex];
+  }
+  document.getElementById("colorCode").innerHTML = hexColorCode;
+  document.getElementsByTagName("body")[0].style.background = "#" + hexColorCode;
+}
